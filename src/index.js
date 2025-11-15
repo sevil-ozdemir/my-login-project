0dfa27c6c85408ad0cc8401d05163cc56694f515
sevil-ozdemir <sevil.tr.ozdemir@gmail.com>
2025-11-15 21:29:42 +0300

    chore: add GitHub repo link to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export const REPO_URL = "https://github.com/<sevil-ozdemir>/my-login-project"
+function cypressTestProjesi() {
+  const checklist = {
+    //github repo adresini gir: Örn: https://github.com/Workintech/hello-world
+    project_url: 'https://github.com/sevil-ozdemir/my-login-project',
+  };
+
+  return checklist;
+}
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = cypressTestProjesi;
+
